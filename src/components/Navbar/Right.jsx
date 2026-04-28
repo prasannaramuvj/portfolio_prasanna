c77575b8c03ab8a007d7428cc0c9242e1419a75a
prasannaramuvj <prasannaramu2004@gmail.com>
2026-04-28 13:30:40 +0530

    fix:right_component

--- src/components/Navbar/Right.jsx
+++ src/components/Navbar/Right.jsx
@@ -12,13 +12,16 @@ const Right = () => {
       transition={{ duration: 0.8, delay: 0.6 }}
     >
       <motion.div
-        className='rotate-90 text-lg lg:text-2xl'
+        className='rotate-90 text-lg lg:text-2xl flex flex-cols justify-center gap-15 '
         initial={{ x: 50, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 0.6, delay: 1.0 }}
       >
         <Link to="/experience">
         <h2 className='transition duration-300 hover:scale-110'>Experience</h2>
+        </Link>
+         <Link to="/education">
+        <h2 className='transition duration-300 hover:scale-110'>Education</h2>
         </Link>
       </motion.div>
     </motion.div>
